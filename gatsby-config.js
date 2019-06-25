ad6da588d46cf5c47dac30f4a650c5485986266f
Horus Lugo <horusgoul@gmail.com>
2019-06-25 18:32:35 +0200

    Demo working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
-	plugins: [
-		'gatsby-plugin-emotion',
-		'gatsby-plugin-react-helmet',
-		{
-			resolve: `gatsby-source-cosmicjs`,
-			options: {
-				bucketSlug: 'minimal-gatsby-localisation-site',
-				objectTypes: ['blog-posts', 'pages'],
-				apiAccess: {
-					read_key: ``,
-				}
-			}
-		}
-	],
-}
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-cosmicjs`,
+      options: {
+        bucketSlug: process.env.COSMIC_BUCKET,
+        objectTypes: ["blog-posts", "pages"],
+        apiAccess: {
+          read_key: process.env.READ_API_KEY
+        }
+      }
+    }
+  ]
+};
